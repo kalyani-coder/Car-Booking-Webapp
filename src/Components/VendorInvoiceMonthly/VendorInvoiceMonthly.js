569252d8a00800a19c9de876ab7e0e0c832eae98
Vedant0370 <vedant.0370@gmail.com>
2023-10-23 12:58:58 +0530

    generate invoice

--- src/Components/VendorInvoiceMonthly/VendorInvoiceMonthly.js
+++ src/Components/VendorInvoiceMonthly/VendorInvoiceMonthly.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import jsPDF from 'jspdf';  // Import jsPDF
 import './VendorInvoiceMonthly.css';
 import Sidebar from '../Sidebar/Sidebar';
-
 
 function VendorInvoiceMonthly() {
   const [formData, setFormData] = useState({
@@ -34,26 +34,33 @@ function VendorInvoiceMonthly() {
   const invoiceItems = [
     { description: 'Item 1', kms: 100, amount: 50, cgst: 2.5, sgst: 2.5, totalAmount: 55 },
     { description: 'Item 2', kms: 200, amount: 75, cgst: 3.75, sgst: 3.75, totalAmount: 82.5 },
-    { description: 'Item 3', kms: 100, amount: 50, cgst: 2.5, sgst: 2.5, totalAmount: 55 },
-    { description: 'Item 4', kms: 200, amount: 75, cgst: 3.75, sgst: 3.75, totalAmount: 82.5 },
-    { description: 'Item 5', kms: 100, amount: 50, cgst: 2.5, sgst: 2.5, totalAmount: 55 },
-    { description: 'Item 6', kms: 200, amount: 75, cgst: 3.75, sgst: 3.75, totalAmount: 82.5 },
-    { description: 'Item 7', kms: 100, amount: 50, cgst: 2.5, sgst: 2.5, totalAmount: 55 },
-    { description: 'Item 8', kms: 200, amount: 75, cgst: 3.75, sgst: 3.75, totalAmount: 82.5 },
     // Add more items as needed
   ];
 
   const [showInvoiceData, setShowInvoiceData] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
+
   const handlePrint = () => {
     setShowInvoiceData(true);
     window.print();
   };
 
   const handleGenerate = () => {
+    const doc = new jsPDF();
+
+    // Add content to the PDF
+    doc.text('Monthly Vendor Invoice', 10, 10);
+    doc.text('Company Name: ' + formData.companyName, 10, 20);
+    doc.text('Company Address: ' + formData.companyAddress, 10, 30);
+    doc.text('GST No: ' + formData.gstno, 10, 40);
+    // Add more content as needed...
+
+    // Save the PDF
+    doc.save('invoice.pdf');
   };
 
   return (
@@ -61,56 +68,38 @@ function VendorInvoiceMonthly() {
       <Sidebar />
 
       <div className="container-vendor-invoice-monthly">
-      <h2 style={{fontSize:"2rem",fontWeight:"bold",marginBottom:"8px"}}>Monthly Vendor Invoice</h2>
+        <h2 style={{ fontSize: "2rem", fontWeight: "bold", marginBottom: "8px" }}>Monthly Vendor Invoice</h2>
         <div className="form-vendor-invoice-monthly">
-          <div className=" pt-4  grid-gap-2  col-6">
-            {/* <label htmlFor="tripid" className="form-label">Trip Id:</label> */}
-            {/* <input className="form-control-vendor-invoice-monthly" type="text" id="tripid" /> */}
-            <label htmlFor="companyname" className="form-label">Company Name:</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Company Name" id="companyname" />
-            <label htmlFor="companyaddress" className="form-label">Company Address:</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Company Address" id="companyaddress" />
-            <label htmlFor="date" className="form-label">Date</label>
-            <input className="form-control-vendor-invoice-monthly" type="date" id="date" />
-            <br />
-            <label htmlFor="contactno" className="form-label">Contact No</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Contact No." id="contactno" />
+          <div className="pt-4 grid-gap-2 col-6">
+            <label htmlFor="companyName" className="form-label">Company Name:</label>
+            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Company Name" id="companyName" name="companyName" onChange={handleChange} />
+            {/* Add similar input elements for other fields */}
           </div>
-          <div className=" pt-4  grid-gap-2  col-6">
-            <label htmlFor="invoiceno" className="form-label">Invoice No:</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Invoice No." id="invoiceno" />
-            <label htmlFor="gstno" className="form-label">GST No</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="GST No." id="gstno" />
-            <label htmlFor="mail" className="form-label">Mail</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Mail" id="mail" />
-
-
-
-
+          <div className="pt-4 grid-gap-2 col-6">
+            <label htmlFor="gstno" className="form-label">GST No:</label>
+            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="GST No." id="gstno" name="gstno" onChange={handleChange} />
+            {/* Add similar input elements for other fields */}
           </div>
+          {/* Add more input elements */}
         </div>
-
 
         <h2 style={{ fontSize: "1.5rem", fontWeight: "bold", marginBottom: "5px" }}>Invoice To :</h2>
-
         <div className="form-vendor-invoice-monthly">
-
-
-          <div className="   grid-gap-2  col-6">
-            <label htmlFor="customername" className="form-label">Vendor Name:</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Vendor Name" id="customername" />
-            <label htmlFor="gastno" className="form-label">GST No:</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="GST No." id="gstno" />
+          <div className="grid-gap-2 col-6">
+            <label htmlFor="customerName" className="form-label">Vendor Name:</label>
+            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Vendor Name" id="customerName" name="customerName" onChange={handleChange} />
+            {/* Add similar input elements for other fields */}
           </div>
-          <div className="  mb-2 grid-gap-2  col-6">
-            <label htmlFor="customeraddress" className="form-label">Vendor Address:</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Vendor Address" id="address" />
-            <label htmlFor="contactno" className="form-label">Contact No</label>
-            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Contact No." id="contactno" />
+          <div className="mb-2 grid-gap-2 col-6">
+            <label htmlFor="customerAddress" className="form-label">Vendor Address:</label>
+            <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Vendor Address" id="customerAddress" name="customerAddress" onChange={handleChange} />
+            {/* Add similar input elements for other fields */}
           </div>
+          {/* Add more input elements */}
         </div>
+
         <div>
-          <table class="invoice-table">
+          <table className="invoice-table">
             <thead>
               <tr>
                 <th>Description</th>
@@ -122,56 +111,44 @@ function VendorInvoiceMonthly() {
               </tr>
             </thead>
             <tbody>
-
-              <tr>
-                <td>Item 1</td>
-                <td>100</td>
-                <td>50</td>
-                <td>2.5%</td>
-                <td>2.5%</td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>Item 2</td>
-                <td>200</td>
-                <td>75</td>
-                <td>3.75%</td>
-                <td>3.75%</td>
-                <td>82.5</td>
-              </tr>
-
+              {invoiceItems.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.description}</td>
+                  <td>{item.kms}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.cgst}</td>
+                  <td>{item.sgst}</td>
+                  <td>{item.totalAmount}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
+
           <div>
             <label>Bank Details:</label>
           </div>
+
           <div className="form-vendor-invoice-monthly">
-            <div className="   grid-gap-2  col-6">
+            <div className="grid-gap-2 col-6">
               <label htmlFor="bankname" className="form-label">Bank Name:</label>
-              <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Bank Name" id="bankname" />
-              <label htmlFor="acnumber" className="form-label">Account Number:</label>
-              <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Account Number" id="acnumber" />
-              <label htmlFor="ifsccode" className="form-label">IFSC Code:</label>
-              <input className="form-control-vendor-invoice-monthly" type="text" placeholder="IFSC Code" id="ifsccode" />
+              <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Bank Name" id="bankname" name="bankname" onChange={handleChange} />
+              {/* Add similar input elements for other fields */}
             </div>
             <div className="grid-gap-2 col-6">
-              <label htmlFor="branchname" className="form-label">Branch Name:</label>
-              <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Branch Name" id="branchname" />
-              <label htmlFor="acholdername" className="form-label">Account Holder Name:</label>
-              <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Account Holder Name" id="acholdername" />
-              <label htmlFor="micrcode" className="form-label">MICR Code:</label>
-              <input className="form-control-vendor-invoice-monthly" placeholder="MICR Code" type="text" id="micrcode" />
+              <label htmlFor="accountNumber" className="form-label">Account Number:</label>
+              <input className="form-control-vendor-invoice-monthly" type="text" placeholder="Account Number" id="accountNumber" name="accountNumber" onChange={handleChange} />
+              {/* Add similar input elements for other fields */}
             </div>
+            {/* Add more input elements */}
           </div>
-          <button type="button-vendor" className="btn btn-danger" onClick={handleGenerate}>
-            Generate
+
+          <button type="button" className="btn btn-danger" onClick={handleGenerate}>
+            Generate PDF
           </button>
         </div>
       </div>
-
     </>
+  );
+}
 
-
-  )
-};
 export default VendorInvoiceMonthly;
