151ea23df2d39070219aef17b8eb8d4fcbfaa1af
GangaPrasad <grakhunde231@gmail.com>
2023-10-25 16:52:40 +0530

    changes in 'viewsharedetails'

--- src/Components/ShareDetails/ViewShareDetails.js
+++ src/Components/ShareDetails/ViewShareDetails.js
@@ -4,17 +4,21 @@ import './ViewShareDetails.css'; // You can create a CSS file for this component
 
 const ViewShareDetails = () => {
   const [shareDetails, setShareDetails] = useState([]);
+  const [filteredShareDetails, setFilteredShareDetails] = useState([]);
   const [error, setError] = useState(null);
+  const [searchDate, setSearchDate] = useState('');
+  const [searchDriverName, setSearchDriverName] = useState('');
 
   useEffect(() => {
     const fetchShareDetails = async () => {
       try {
         const response = await fetch('http://localhost:7000/api/share-details');
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw Error('Network response was not ok');
         }
         const data = await response.json();
         setShareDetails(data);
+        setFilteredShareDetails(data); // Initialize filtered data with all share details
       } catch (error) {
         setError('Error fetching share details: ' + error.message);
       }
@@ -23,17 +27,46 @@ const ViewShareDetails = () => {
     fetchShareDetails();
   }, []);
 
+  // Function to filter share details based on search criteria
+  const filterShareDetails = () => {
+    const filteredData = shareDetails.filter((shareDetail) => {
+      const dateMatches = shareDetail.date.includes(searchDate);
+      const driverNameMatches = shareDetail.drivername.toLowerCase().includes(searchDriverName.toLowerCase());
+      return dateMatches && driverNameMatches;
+    });
+
+    setFilteredShareDetails(filteredData);
+  };
+
+  useEffect(() => {
+    filterShareDetails();
+  }, [searchDate, searchDriverName]);
+
   return (
     <>
       <Sidebar />
       <div className="share-details-container">
         <div className="share-details-main-container">
           <h1>Share Details</h1>
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search by date"
+              value={searchDate}
+              onChange={(e) => setSearchDate(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Search by driver name"
+              value={searchDriverName}
+              onChange={(e) => setSearchDriverName(e.target.value)}
+            />
+          </div>
           {error ? (
             <p>Error: {error}</p>
           ) : (
             <div className="row">
-              {shareDetails.map((shareDetail) => (
+              {filteredShareDetails.map((shareDetail) => (
                 <div key={shareDetail._id} className="col-4">
                   <div className="card mb-3">
                     <div className="card-body">
