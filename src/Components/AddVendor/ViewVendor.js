fa59318be35c7c14f04d63f46688263442d04052
Vedant0370 <vedant.0370@gmail.com>
2023-10-26 16:28:16 +0530

    search bar added

--- src/Components/AddVendor/ViewVendor.js
+++ src/Components/AddVendor/ViewVendor.js
@@ -4,13 +4,14 @@ import './ViewVender.css'; // Make sure you have a CSS file for this component
 
 const ViewVendor = () => {
   const [vendors, setVendors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchVendors = async () => {
       try {
         const response = await fetch('http://localhost:7000/api/add-venders');
         if (!response.ok) {
-          throw  Error('Network response was not ok');
+          throw Error('Network response was not ok');
         }
         const data = await response.json();
         setVendors(data);
@@ -22,18 +23,41 @@ const ViewVendor = () => {
     fetchVendors();
   }, []);
 
+  const filteredVendors = vendors.filter((vendor) => {
+    const venderName = vendor.vender_Name || '';
+    const companyName = vendor.company_Name || '';
+    return (
+      venderName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      companyName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
+
   return (
     <>
       <Sidebar />
       <div className="vendor-Add-container">
         <div className="vendor-main-container">
           <h1 className="text-2xl font-semibold mb-4">Vendors</h1>
+          <input
+            type="search"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full py-2 px-4 border rounded-lg shadow-md mb-4"
+          />
           <div className="grid grid-cols-3 gap-4">
-            {vendors.map((vendor) => (
-              <div key={vendor._id} className="custom-card bg-white shadow-md rounded-lg overflow-hidden">
+            {filteredVendors.map((vendor) => (
+              <div
+                key={vendor._id}
+                className="custom-card bg-white shadow-md rounded-lg overflow-hidden"
+              >
                 <div className="custom-card-body p-4">
-                  <h5 className="custom-card-title text-lg font-semibold mb-2">Vendor Name: {vendor.vender_Name}</h5>
-                  <p className="custom-card-subtitle mb-2">Company Name: {vendor.company_Name}</p>
+                  <h5 className="custom-card-title text-lg font-semibold mb-2">
+                    Vendor Name: {vendor.vender_Name}
+                  </h5>
+                  <p className="custom-card-subtitle mb-2">
+                    Company Name: {vendor.company_Name}
+                  </p>
                   <p className="custom-card-subtitle mb-2">GST No: {vendor.GST_No}</p>
                   <p className="custom-card-subtitle mb-2">Mobile: {vendor.vender_Mobile}</p>
                   <p className="custom-card-subtitle mb-2">Email: {vendor.Vender_Email}</p>
