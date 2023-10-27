614f8b463165decbeb97b7a73761f269e17d2dea
Vedant0370 <vedant.0370@gmail.com>
2023-10-27 12:43:49 +0530

    api for vender payment

--- src/Components/VendorPayment/VendorPayment.js
+++ src/Components/VendorPayment/VendorPayment.js
@@ -1,152 +1,257 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './VendorPayment.css';
 import Sidebar from '../Sidebar/Sidebar';
+
 function VendorPayment() {
+
+  const [formData, setFormData] = useState({
+    company_Name: "",
+    GST_No: "",
+    vender_Name: "",
+    mobile_Number: "",
+    payment: "",
+    amount: "",
+    tds: "",
+    total_Amount: "",
+    paid_Amount: "",
+    remaining_Amount: "",
+    payment_Method: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+
+    for (const key in formData) {
+      if (formData[key] === "") {
+        window.alert("All fields are required");
+        return;
+      }
+    }
+
+    const response = await fetch("http://localhost:7000/api/vender-payment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+      console.log("Data posted successfully!");
+      window.alert('Data post Successfully')
+    } else {
+      console.error("Error posting data:", response.statusText);
+    }
+  };
+
   return (
     <>
-    <Sidebar/>
-    <div className="container-fluid ">
+      <Sidebar />
+      <div className="container-fluid ">
+        <div className="form-body" >
+          <div className="card-1">
+            <div className="row justify-content-center">
+              <div className="col-md-10">
+                <div className="card-body mt-5">
+                  <form onSubmit={handleSubmit}>
 
-    <div className="form-body" >
-  <div className="card-1">
-  <div className="row justify-content-center">
-  <div className="col-md-10">
-  <div className="card-body mt-5">
-  <form>
+                    <div className="row g-2">
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="company_Name">Company Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="company_Name"
+                            name="company_Name"
+                            placeholder="Enter Company Name"
+                            value={formData.company_Name}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="GST_No">GST No</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="GST_No"
+                            name="GST_No"
+                            placeholder="Enter GST No"
+                            value={formData.GST_No}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
 
-  <div className="row g-2">
-    <div className="col-md">
-      <div className="form-group">
-      <label >Company Name</label>
-        <input type="text" className="form-control" placeholder="Enter Company Name" />
+                    <div className="row g-2">
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="vender_Name">Vendor Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="vender_Name"
+                            name="vender_Name"
+                            placeholder="Enter Vendor Name"
+                            value={formData.vender_Name}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="mobile_Number">Mobile Number</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="mobile_Number"
+                            name="mobile_Number"
+                            placeholder="Enter Mobile Number"
+                            value={formData.mobile_Number}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
 
+                    <div className="row g-2">
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="payment">Payment</label>
+                          <select
+                            className="form-control"
+                            id="payment"
+                            name="payment"
+                            value={formData.payment}
+                            onChange={handleChange}
+                          >
+                            <option value="">Select Payment</option>
+                            <option value="Partial">Partial</option>
+                            <option value="Full Payment">Full Payment</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="amount">Amount</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="amount"
+                            name="amount"
+                            placeholder="Enter Amount"
+                            value={formData.amount}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="row g-2">
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="tds">TDS 1%</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="tds"
+                            name="tds"
+                            placeholder="Enter TDS 1% Amount"
+                            value={formData.tds}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="total_Amount">Total Amount</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="total_Amount"
+                            name="total_Amount"
+                            placeholder="Enter Total Amount"
+                            value={formData.total_Amount}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="row g-2">
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="paid_Amount">Paid Amount</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="paid_Amount"
+                            name="paid_Amount"
+                            placeholder="Enter Paid Amount"
+                            value={formData.paid_Amount}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                      <div className="col-md">
+                        <div className="form-group">
+                          <label htmlFor="remaining_Amount">Remaining Amount</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="remaining_Amount"
+                            name="remaining_Amount"
+                            placeholder="Enter Remaining Amount"
+                            value={formData.remaining_Amount}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="payment_Method">Payment Method</label>
+                      <select
+                        className="form-control"
+                        id="payment_Method"
+                        name="payment_Method"
+                        value={formData.payment_Method}
+                        onChange={handleChange}
+                      >
+                        <option value="">Select Payment Method</option>
+                        <option value="Bank Transfer">Bank Transfer</option>
+                        <option value="Cash">Cash</option>
+                        <option value="Google Pay">Google Pay</option>
+                        <option value="Paytm">Paytm</option>
+                        <option value="Phone Pay">Phone Pay</option>
+                      </select>
+                    </div>
+
+                    <br />
+                    <button type="submit" className="btn btn-danger">
+                      Save
+                    </button>
+
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="col-md">
-      <div className="form-group">
-      <label >GST No</label>
-        <input type="text" className="form-control"  placeholder="Enter GST No" />
-
-      </div>
-    </div>
-  </div>
-
-  <div className="row g-2">
-    <div className="col-md">
-      <div className="form-group">
-      <label >Vendor Name</label>
-        <input type="text" className="form-control" placeholder="Enter Vendor Name" />
-
-      </div>
-    </div>
-    <div className="col-md">
-      <div className="form-group">
-      <label >Mobile Number</label>
-        <input type="text" className="form-control"  placeholder="Enter Mobile Number" />
-
-      </div>
-    </div>
-  </div>
-  <div className="row g-2">
-    <div className="col-md">
-      <div className="form-group">
-      <label >Payment</label>
-      <select
-                  className="form-control"
-                  name="title"
-                  id="title"
-                  // onChange={handleChange}
-                  // value={formData.title}
-                > 
-                <option value="Payment">Payment</option>
-                  <option value="Partial">Partial</option>
-                  <option value="Full Payment">Full Payment</option>
-                  {/* Add other vehicle options */}
-                </select>
-
-      </div>
-    </div>
-    <div className="col-md">
-      <div className="form-group">
-      <label >Amount</label>
-        <input type="text" className="form-control"  placeholder="Enter your Amount" />
-
-      </div>
-    </div>
-  </div>
-
-  <div className="row g-2">
-    <div className="col-md">
-      <div className="form-group">
-      <label >TDS 1%</label>
-        <input type="text" className="form-control" placeholder="Enter  TDS 1% Amonut" />
-
-      </div>
-    </div>
-    <div className="col-md">
-      <div className="form-group">
-      <label > Total Amount</label>
-        <input type="text" className="form-control"  placeholder="Enter total Amount" />
-
-      </div>
-    </div>
-  </div>
-
-  <div className="row g-2">
-    <div className="col-md">
-      <div className="form-group">
-      <label >Paid Amount</label>
-        <input type="text" className="form-control" placeholder="Enter Paid Amount" />
-
-      </div>
-    </div>
-    <div className="col-md">
-      <div className="form-group">
-      <label >Remaining Amount</label>
-        <input type="text" className="form-control"  placeholder="Enter Remaining Amount" />
-
-      </div>
-    </div>
-  </div>
-
-  
-  {/* </div> */}
-  <div className="form-group">
-  <label> Payment Method </label>
-  <select
-                  className="form-control"
-                  name="paymentmethod"
-                  id="paymentmethod"
-                  // onChange={handleChange}
-                  // value={formData.paymentmethod}
-                >
-                  <option value="">Payment Method</option>
-                  <option value="Bank Transfer">Bank Transfer</option>
-                  <option value="Cash">Cash</option>
-                  <option value="Google Pay">Google Pay</option>
-                  <option value="Paytm">Paytm</option>
-                  <option value="Phone Pay">Phone Pay</option>
-                </select>
-
-  </div>
-  <br></br>
-  <button id="btn1" className="btn btn-danger " >
-  Edit
-  </button>
-  <button id="btn2" className="btn btn-danger " >
-  Save
-  </button>
-  <button id="btn3" className="btn btn-danger " >
-Print
-</button>
-  </form>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
-  </>
-  )};
+    </>
+  )
+}
 
 export default VendorPayment;
