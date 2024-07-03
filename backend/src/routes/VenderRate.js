874ac747dcbe3ff725c1304f80992f71e9e9ad1d
Vedant0370 <vedant.0370@gmail.com>
2024-07-03 12:52:07 +0530

    update VenderRate.js

--- backend/src/routes/VenderRate.js
+++ backend/src/routes/VenderRate.js
@@ -4,74 +4,86 @@ const newRateSchemaVender = require('../models/VenderRateModel')
 const router = express.Router()
 
 // GET METHOD 
-router.get('/' , async(req , res) => {
-    try{
-       const AddVenders = await newRateSchemaVender.find()
+router.get('/', async (req, res) => {
+    try {
+        const AddVenders = await newRateSchemaVender.find()
         res.status(201).json(AddVenders)
 
-    }catch(e){
-        res.status(404).json({message : "Can not get venders"})
+    } catch (e) {
+        res.status(404).json({ message: "Can not get venders" })
     }
 })
 
 // GET BY ID 
-router.get('/:id' , async(req, res) => {
+router.get('/:id', async (req, res) => {
 
-        const AddVendersId = req.params.id 
+    const AddVendersId = req.params.id
 
-        try{
+    try {
 
-            const AddVenders = await newRateSchemaVender.findById(AddVendersId)
-            if(!AddVenders){
-                return res.status(404).json({message : "venders Not found"})
-            }
-            res.json(AddVenders)
-
-        }catch(e){
-            res.status(404).json({message : "venders Not Found"})
+        const AddVenders = await newRateSchemaVender.findById(AddVendersId)
+        if (!AddVenders) {
+            return res.status(404).json({ message: "venders Not found" })
         }
-    
+        res.json(AddVenders)
+
+    } catch (e) {
+        res.status(404).json({ message: "venders Not Found" })
+    }
+
 })
 
 // POST METHOD 
-router.post('/' , async(req, res) => {
+router.post('/', async (req, res) => {
 
-    try{
+    try {
         const AddVenders = new newRateSchemaVender(req.body)
-      await AddVenders.save()
-        res.status(201).json({message : "Data post Successfully"})
+        await AddVenders.save()
+        res.status(201).json({ message: "Data post Successfully" })
 
-    }catch(e){
-        res.status(404).json({message : "Can not post venders"})
+    } catch (e) {
+        res.status(404).json({ message: "Can not post venders" })
     }
 })
 
 // PATCH METHOD 
-router.patch('/:id' , async(req, res) => {
+router.patch('/:id', async (req, res) => {
 
     const AddVendersId = req.params.id
-    
-    try{
-        const UpdatedAddVenders = await newRateSchemaVender.findByIdAndUpdate(AddVendersId , req.body ,{
-            new : true
-        })
-        res.status(201).json({message : "venders Successfully updated "})
 
-    }catch(e){
-        res.status(404).json({message : "Can not patch venders"})
+    try {
+        const UpdatedAddVenders = await newRateSchemaVender.findByIdAndUpdate(AddVendersId, req.body, {
+            new: true
+        })
+        res.status(201).json({ message: "venders Successfully updated " })
+
+    } catch (e) {
+        res.status(404).json({ message: "Can not patch venders" })
     }
 })
 
 // DELETE METHOD
-router.delete('/:id' , async(req, res) => {
+router.delete('/:id', async (req, res) => {
 
-    const AddVendersId = req.params.id 
+    const AddVendersId = req.params.id
 
-    try{
+    try {
         const deletedCustomeEnquiry = await newRateSchemaVender.findByIdAndDelete(AddVendersId)
-        res.status(201).json({message : "venders Successfully Deleted "})
-    }catch(e){
-        res.status(404).json({message : "Can not found" , e})
+        res.status(201).json({ message: "venders Successfully Deleted " })
+    } catch (e) {
+        res.status(404).json({ message: "Can not found", e })
+    }
+})
+
+router.get("/venderid/:vendorId", async (req, res) => {
+    try {
+        const data = await newRateSchemaVender.find({vender_id : req.params.vendorId})
+        if(data === 0){
+            return res.status(404).json({message : "Data not found for this Id"})
+        }
+        res.status(201).json(data)
+    } catch (e) {
+        res.status(500).json({ message: "Internla server error " })
     }
 })
 
